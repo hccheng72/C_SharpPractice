31e942ac59c9c10033b970e2cd04b6f9e3f5fb14
hccheng72 <70727602+hccheng72@users.noreply.github.com>
2021-11-18 22:54:21 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Exercise3
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Enter a string: ");
+            string input = Console.ReadLine();
+            Console.WriteLine("The reversed string is " + (new StringManipulation()).ReverseString(input));
+        }
+
+        
+    }
+
+    class StringManipulation 
+    {
+        public string ReverseString(string s)
+        {
+            string ans = "";
+            for (int i = s.Length - 1; i > -1; i--)
+            {
+                ans += s[i];
+            }
+            return ans;
+        }
+    }
+}
+
+
