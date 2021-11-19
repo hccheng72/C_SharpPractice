5b430c15ccdb15424662d956aa1eb9ad3d9cc424
hccheng72 <70727602+hccheng72@users.noreply.github.com>
2021-11-18 22:55:38 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Exercise5
+{
+    class Program
+    {
+        static void Main()
+        {
+            int row;
+            do
+            {
+                Console.Write("Enter the number of rows (positive number): ");
+                row = Convert.ToInt32(Console.ReadLine());
+            } while (row <= 0);
+
+            int n = 1;
+            while (n <= row)
+            {
+                string s = "";
+                for (int i = 0; i < n; i++)
+                {
+                    if (i % 2 == 0)
+                        s += "1";
+                    else
+                        s += "0";
+                }
+                n++;
+                Console.WriteLine(s);
+            }
+            
+
+        }
+    }
+}
