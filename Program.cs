5301a1d52e708f8cc3fda066899cfda65c4ab952
hccheng72 <70727602+hccheng72@users.noreply.github.com>
2021-11-18 22:58:04 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CAssignment cAssignment = new CAssignment();
+            int[] arr = { 1, 2, 3 };
+            cAssignment.RepeatArray(arr);
+        }
+    }
+
+
+    class CAssignment
+    {
+        public void RepeatArray(int[] arr)
+        {
+            int[] doubleArr = arr.Concat(arr).ToArray();
+            foreach (int n in doubleArr)
+                Console.Write(n + " ");
+        }
+    }
+}
